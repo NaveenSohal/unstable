bacd0e414282988b9457ade52f4b896b0ddf9f9d
NAVEEN SOHAL <90239182+NaveenSohal@users.noreply.github.com>
2024-03-25 14:45:40 +0530

    Update launch.py with f code

--- launch.py
+++ launch.py
@@ -1,3 +1,20 @@
+import os
+import ssl
+import sys
+
+print('[System ARGV] ' + str(sys.argv))
+
+root = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(root)
+os.chdir(root)
+
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
+if "GRADIO_SERVER_PORT" not in os.environ:
+    os.environ["GRADIO_SERVER_PORT"] = "7865"
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
 from modules import launch_utils
 
 args = launch_utils.args
